5c7d5cfe3797d5de6dca2cdbf741321ef62e89cc
umangdosi07 <umangdosi.in@gmail.com>
2026-01-07 06:40:10 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from routers.donations import router as donations_router
 from routers.auth import router as auth_router
 from routers.users import router as users_router
+from routers.programs import router as programs_router
 
 app = FastAPI()
 
@@ -13,3 +14,4 @@ def root():
 app.include_router(donations_router, prefix="/api")
 app.include_router(auth_router, prefix="/api")
 app.include_router(users_router, prefix="/api")
+app.include_router(programs_router, prefix="/api")
