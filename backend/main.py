386a825dd7f93641d12f4a4f9e2551a00d477e31
umangdosi07 <umangdosi.in@gmail.com>
2026-01-07 06:25:20 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
+from routers.donations import router as donations_router
+
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+# add ONLY donations router
+app.include_router(donations_router, prefix="/api")
